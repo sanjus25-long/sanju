e705ebb9f157ff1b3a4ffa75b610fbe3a9e61852
sanjus25-long <Dtc0823039@nttf.co.in>
2025-09-09 12:04:43 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -2,6 +2,8 @@ public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Welcome to nttf");
+        System.out.println("Welcome to nec nttf");
     }
 }
+
 
